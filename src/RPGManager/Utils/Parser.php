3c2f304f4b684dfed86ed92d7b68044555f9a749
Henri Chabannes <henri.de-chabannes@supinternet.fr>
2017-12-07 13:12:07 +0100

    Added Parser and Template; Updated autoload in composer.json

---
+++ src/RPGManager/Utils/Parser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace RPGManager\Utils;
+
+class Parser
+{
+    private $config;
+
+    public static function generateModelsDb($config)
+    {
+        var_dump($config);
+        echo "coucoi";
+        return true;
+    }
+}
