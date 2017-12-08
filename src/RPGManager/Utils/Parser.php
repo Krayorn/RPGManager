05d15169249c645ceba2d757efe54120712bca86
Emmanuel Micard <emmanuel.micard@supinternet.fr>
2017-12-08 16:16:43 +0100

    Added getInstance in Parser.php; Started parser function linked with settings.php

--- src/RPGManager/Utils/Parser.php
+++ src/RPGManager/Utils/Parser.php
@@ -4,9 +4,7 @@ namespace RPGManager\Utils;
 
 use RPGManager\Template;
 
-class Parser extends Template
-{
-    private $config;
+class Parser extends Template {
     private static $instance = null;
 
     public static function getInstance() {
@@ -16,13 +14,26 @@ class Parser extends Template
         return self::$instance;
     }
 
-    public static function generateModelsDb($config)
-    {
+    public static function generateModelsDb($gameConfig, $settings) {
         $parser = Parser::getInstance();
 
         $line = "generateModelsDb() => Parsing models";
         $parser->writeAccessLog($line);
 
+        $items = $gameConfig["items"];
+        $monsters = $gameConfig["monsters"];
+        $npcs = $gameConfig["npcs"];
+        $characters = $gameConfig["characters"];
+        $places = $gameConfig["places"];
+
+        if (isset($settings["turnItemsOn"]) && $settings["turnItemsOn"]){
+            $parser->parseItems($items);
+        }
+
         return true;
+    }
+
+    private function parseItems($items) {
+        
     }
 }
