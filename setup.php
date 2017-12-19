f3dfd87dee0e8d033c228c51a0b14d5655498ebd
Emmanuel Micard <emmanuel.micard@supinternet.fr>
2017-12-19 15:59:47 +0100

    Removed configs to create db

--- setup.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$createSqlDB = shell_exec('vendor\bin\doctrine orm:schema-tool:create --dump-sql');
-echo $createSqlDB;
-$createDB = shell_exec('vendor\bin\doctrine orm:schema-tool:create');
-echo "Done!";
